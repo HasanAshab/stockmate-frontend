5c0c9af80509767cca5e22cab55b032a9a83ed9a
Hasan Ashab <hasanashab.18205@gmail.com>
2026-06-06 10:21:52 +0600

    backup

--- src/api/axios.js
+++ src/api/axios.js
@@ -41,8 +41,11 @@ api.interceptors.response.use((response) => {
   return response;
 }, (error) => {
   if (error.response?.status === 401 || error.response?.status === 419) {
+    localStorage.removeItem('authToken');
     localStorage.removeItem('authUser');
-    window.location.href = '/login';
+    if (window.location.pathname !== '/login') {
+      window.location.href = '/login';
+    }
   }
   return Promise.reject(error);
 });
